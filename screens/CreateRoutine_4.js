2a48e4a33bd7b6c82dd1785130019d18189982dd
Choi Seho <kindseho@naver.com>
2023-07-21 14:30:39 +0900

    feat: CreateRoutine_4 dayItem disable 설정 및 allDayButton 활성화

--- screens/CreateRoutine_4.js
+++ screens/CreateRoutine_4.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
 
 export default function CreateRoutine_4({ navigation }) {
+  const [select, SetSelect] = useState(false);
+  const [allDay, SetAllDay] = useState(false);
+  const [days, setDays] = useState([
+    { id: 1, name: "일", selected: false },
+    { id: 2, name: "월", selected: false },
+    { id: 3, name: "화", selected: false },
+    { id: 4, name: "수", selected: false },
+    { id: 5, name: "목", selected: false },
+    { id: 6, name: "금", selected: false },
+    { id: 7, name: "토", selected: false },
+  ]);
+  const onDayPress = (id) => {
+    setDays((prevDays) =>
+      prevDays.map((day) =>
+        day.id === id ? { ...day, selected: !day.selected } : day
+      )
+    );
+  };
+  useEffect(() => {
+    SetSelect(
+      days[0].selected ||
+        days[1].selected ||
+        days[2].selected ||
+        days[3].selected ||
+        days[4].selected ||
+        days[5].selected ||
+        days[6].selected
+    );
+  }, [days]);
+  const AllDayPress = () => {
+    SetAllDay(!allDay);
+  };
+  useEffect(() => {
+    setDays((prevDays) =>
+      prevDays.map((day) =>
+        allDay ? { ...day, selected: true } : { ...day, selected: false }
+      )
+    );
+  }, [allDay]);
   return (
     <Container>
       <StackBar>
@@ -12,33 +51,23 @@ export default function CreateRoutine_4({ navigation }) {
         <SubTitle>마이루틴에서 언제든지 변경할 수 있어요.</SubTitle>
       </TitleContainer>
       <DayContainer>
-        <DayItem>
-          <DayName>월</DayName>
-        </DayItem>
-        <DayItem>
-          <DayName>월</DayName>
-        </DayItem>
-        <DayItem>
-          <DayName>월</DayName>
-        </DayItem>
-        <DayItem>
-          <DayName>월</DayName>
-        </DayItem>
-        <DayItem>
-          <DayName>월</DayName>
-        </DayItem>
-        <DayItem>
-          <DayName>월</DayName>
-        </DayItem>
-        <DayItem>
-          <DayName>월</DayName>
-        </DayItem>
+        {days.map((day) => (
+          <DayItem
+            key={day.id}
+            onPress={() => onDayPress(day.id)}
+            style={{ backgroundColor: day.selected ? "#757575" : "white" }}
+          >
+            <DayName style={{ color: day.selected ? "white" : "#757575" }}>
+              {day.name}
+            </DayName>
+          </DayItem>
+        ))}
       </DayContainer>
-      <AllDayButton>
+      <AllDayButton isActive={allDay} onPress={AllDayPress}>
         <AllDayText>매일 운동할래요</AllDayText>
       </AllDayButton>
-      <NextButton>
-        <ButtonText>선택 완료</ButtonText>
+      <NextButton isActive={select} disabled={!select}>
+        <ButtonText isActive={select}>선택 완료</ButtonText>
       </NextButton>
     </Container>
   );
@@ -91,11 +120,13 @@ const DayItem = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
 `;
-const DayName = styled.Text``;
+const DayName = styled.Text`
+  color: #757575;
+`;
 const AllDayButton = styled.TouchableOpacity`
   width: 110px;
   height: 40px;
-  background-color: #dddddd;
+  background-color: ${(props) => (props.isActive ? "#BFBFBF" : "#DDDDDD")};
   margin-bottom: 50px;
   border-radius: 100px;
   align-items: center;
@@ -109,8 +140,10 @@ const NextButton = styled.TouchableOpacity`
   height: 52px;
   align-items: center;
   justify-content: center;
-  background-color: #dddddd;
+  background-color: ${(props) => (props.isActive ? "#BFBFBF" : "#DDDDDD")};
   border-radius: 10px;
   margin-bottom: 45px;
 `;
-const ButtonText = styled.Text``;
+const ButtonText = styled.Text`
+  color: ${(props) => (props.isActive ? "black" : "#757575")};
+`;
