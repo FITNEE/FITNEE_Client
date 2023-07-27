fde0ced88bd9c97547d6e0292c4747e01fda8e80
leejin_rho <tbdpapdl@gmail.com>
2023-07-27 23:46:36 +0900

    .

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
+import CreateRoutineNav from './navigation/CreateRoutineNav';
 import AppLoading from 'expo-app-loading';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+// import OnBoardingNav from './navigation/OnBoardingNav';
+import MyPageNav from './navigators/MyPageNav';
+// import LoggedInNav from './navigation/LoggedInNav';
+import { Context, ContextProvider } from './components/ContextProvider';
 import OnBoardingNav from './navigators/OnBoardingNav';
 import LoggedInNav from './navigators/LoggedInNav';
 import { AppContext } from './components/ContextProvider';
 import { useState } from 'react';
-import ExerciseCourseNav from './navigators/ExerciseCourseNav';
 
 const MyTheme = {
 	...DefaultTheme,
@@ -17,44 +21,39 @@ const MyTheme = {
 export default function App() {
 	const [loading, setLoading] = useState(true);
 	const [loggedIn, setLoggedIn] = useState(true);
-	const [colorMode, setColorMode] = useState(false);
 	const onFinish = () => setLoading(false);
 	const toggleLogin = () => {
 		setLoggedIn(!loggedIn);
 		console.log(loggedIn);
 	};
-	const toggleColorMode = () => {
-		setColorMode(!colorMode);
-	};
 	const userSettings = {
 		toggleLogin,
-		toggleColorMode,
 		loggedIn,
-		colorMode,
 	};
 
 	const preload = async () => {
 		// const token = await AsyncStorage.getItem("token");
 		// if (token) {
-		//   isLoggedInVar(true);
-		//   tokenVar(token);
+		// setLoggedIn(true);
 		// }
 	};
 
-	if (loading) {
-		return (
-			<AppLoading
-				startAsync={preload}
-				onError={console.warn}
-				onFinish={onFinish}
-			/>
-		);
-	}
+	// if (loading) {
+	//   return (
+	//     <AppLoading
+	//       startAsync={preload}
+	//       onError={console.warn}
+	//       onFinish={onFinish}
+	//     />
+	//   );
+	// }
+
+	const [isDark, setIsDark] = useState(false);
+
 	return (
-		<AppContext.Provider value={userSettings}>
+		<AppContext.Provider value={{ isDark, setIsDark, toggleLogin, loggedIn }}>
 			<NavigationContainer theme={MyTheme}>
-				{/* {loggedIn ? <LoggedInNav /> : <OnBoardingNav />} */}
-				{loggedIn ? <ExerciseCourseNav /> : <ExerciseCourseNav />}
+				<LoggedInNav />
 			</NavigationContainer>
 		</AppContext.Provider>
 	);
