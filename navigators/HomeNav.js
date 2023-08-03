7be14d17d78697bf9588f14eb371d0d92e0a1964
NeoSelf1 <neoself1105@gmail.com>
2023-08-03 19:30:35 +0900

    init

--- navigators/HomeNav.js
+++ navigators/HomeNav.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/Home";
-import Test from "../screens/MyRoutine/Test";
 
 const Stack = createStackNavigator();
 
