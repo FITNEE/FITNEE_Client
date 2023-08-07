78b988459c68ecb09f20c4957c0a43730c471d52
joohee <2170095@ewhain.net>
2023-08-07 10:28:21 +0900

    디자인 수정

--- screens/MyPage/UserInfo.js
+++ screens/MyPage/UserInfo.js
@@ -2,143 +2,157 @@ import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native";
 import { styled } from "styled-components/native";
 import { colors } from "../../colors";
+import axios from "axios";
+import { Dimensions } from "react-native";
+import { ScreenWidth } from "../../Shared";
 
-
-    const Container = styled.View`
-        background-color: #fff;
-        height: 100%;
-    `;
-    const Profile = styled.View`
-        align-items: center;
-        margin-top: 35px;
-        margin-bottom: 32px;
-    `;
-    const ProfileImage = styled.TouchableOpacity`
-        width: 88px;
-        height: 88px;
-        background-color: ${colors.grey_2};
-        border-radius: 88px;
-    `;
-    const Block = styled.View`
-        flex-direction: row;
-        padding: 15px 24px;
-    `;
-    const BlockTitle = styled.Text`
-        width: 100px;
-        font-size: 17px;
-        font-style: normal;
-        font-weight: 400;
-        line-height: 25.5px;
-    `;
-    const BlockContent = styled.Text`
-        width: 224px;
-        text-align: right;
-        font-size: 17px;
-        font-style: normal;
-        font-weight: 400;
-        line-height: 25.5px;
-        color: ${colors.grey_5};
-    `;
-    const Bar = styled.View`
-        height: 16px;
-        background-color: #f3f3f3;
-    `;
-    const MiniBlock = styled.View`
-        height: 48px;
-        justify-content: center;
-    `;
-    const Click = styled.TouchableOpacity`
-        margin-right: 24px;
-    `;
-    const ClickText = styled.Text`
-        text-align: right;
-        font-size: 13px;
-        font-style: normal;
-        font-weight: 400;
-        line-height: 19.5px;
-        text-decoration-line: underline;
-    `;
+const Container = styled.View`
+  background-color: #fff;
+  height: 100%;
+`;
+const Profile = styled.View`
+  align-items: center;
+  margin-top: 35px;
+  margin-bottom: 32px;
+`;
+const ProfileImage = styled.TouchableOpacity`
+  width: 88px;
+  height: 88px;
+  background-color: ${colors.grey_2};
+  border-radius: 88px;
+`;
+const Block = styled.View`
+  flex-direction: row;
+  padding: 15px 24px;
+`;
+const BlockTitle = styled.Text`
+  width: 100px;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 25.5px;
+`;
+const BlockContent = styled.Text`
+  width: ${ScreenWidth - 148}px;
+  text-align: right;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 25.5px;
+  color: ${colors.grey_5};
+`;
+const NickBlock = styled.TouchableOpacity`
+  flex-direction: row;
+  padding: 15px 24px;
+`;
+const NickContent = styled.View`
+  width: ${ScreenWidth - 148}px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+`;
+const NickText = styled.Text`
+  text-align: right;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 25.5px;
+  color: ${colors.grey_5};
+`;
+const NickIcon = styled.Image`
+  width: 20px;
+  height: 20px;
+  margin-left: 8px;
+  background-color: pink;
+`;
+const Bar = styled.View`
+  height: 16px;
+  background-color: #f3f3f3;
+`;
+const MiniBlock = styled.View`
+  height: 48px;
+  justify-content: center;
+`;
+const Click = styled.TouchableOpacity`
+  margin-right: 24px;
+`;
+const ClickText = styled.Text`
+  text-align: right;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 19.5px;
+  text-decoration-line: underline;
+`;
 
 export default function UserInfo({ navigation }) {
+  const [userInfo, setUserInfo] = useState([
+    {
+      birthYear: "",
+      userId: "",
+      userNickname: "",
+    },
+  ]);
 
-    const [userData, setUserData] = useState([]);
-    const getUserInfoData = async () => {
+  const getUserInfoData = async () => {
     try {
-        let url = "https://gpthealth.shop/";
-        let detailAPI = `app/mypage/userinfo`;
-        const response = await axios.get(
-        url + detailAPI
-        );
-        const result = response.data;
-        return result;
+      let url = "https://gpthealth.shop/";
+      let detailAPI = `app/mypage/userinfo`;
+      const response = await axios.get(url + detailAPI);
+      const result = response.data;
+      return result;
     } catch (error) {
-        console.error("Failed to fetch data:", error);
+      console.error("Failed to fetch data:", error);
     }
-    };
-    /*
-    const updatePWD = async (newPW) => {
-    try {
-        let url = "https://gpthealth.shop/";
-        let detailAPI = `app/mypage/updatepwd`;
-        const response = axios.put(url + detailAPI, {
-        userPw: newPW,
-        });
+  };
 
-        //   const result = response.data;
-        return response;
-    } catch (error) {
-        console.error("Failed to fetch data:", error);
-    }
-    };
-    */
+  useEffect(() => {
+    getUserInfoData().then((result) => {
+      setUserInfo(result.result);
+    });
+  }, []);
 
-    useEffect(() => {
-    // updatePWD().then((result) => {
-    //   console.log(result.data);
-    // });
-    getUserInfoData().then((response) => {
-        //setData(response);
-        console.log(response);
-        });
-        }, []);
-    
-    const USER_DATA = [
-        {
-            id: 1,
-            username: '초코맛 프로틴',
-            birth: 1998,
-            email: 'protein012@gamil.com'
-        }
-    ];
+  const getUserName = userInfo[0].userNickname;
+  const getBirthYear = userInfo[0].birthYear.toString();
+  const getUserId = userInfo[0].userId;
 
-    const username = useState('초코맛 프로틴');
-    const birth = useState('1998');
-    const email = useState('protein012@gmail.com');
-
-    return (
+  return (
     <SafeAreaView>
-    <Container>
-        <Profile><ProfileImage></ProfileImage></Profile>
+      <Container>
+        <Profile>
+          <ProfileImage></ProfileImage>
+        </Profile>
+        <NickBlock>
+          <BlockTitle>닉네임</BlockTitle>
+          <NickContent>
+            <NickText>{getUserName}</NickText>
+            <NickIcon />
+          </NickContent>
+        </NickBlock>
         <Block>
-            <BlockTitle>닉네임</BlockTitle>
-            <BlockContent>{username}</BlockContent>
+          <BlockTitle>출생년도</BlockTitle>
+          <BlockContent>{getBirthYear}</BlockContent>
         </Block>
         <Block>
-            <BlockTitle>출생년도</BlockTitle>
-            <BlockContent>{birth}</BlockContent>
+          <BlockTitle>이메일 주소</BlockTitle>
+          <BlockContent>{getUserId}</BlockContent>
         </Block>
-        <Block>
-            <BlockTitle>이메일 주소</BlockTitle>
-            <BlockContent>{email}</BlockContent>
-        </Block>
-        <Bar/>
+        <Bar />
         <MiniBlock>
-            <Click onPress={() => { navigation.navigate("EditPW")}}><ClickText>비밀번호 수정</ClickText></Click>
+          <Click
+            onPress={() => {
+              navigation.navigate("EditPW");
+            }}
+          >
+            <ClickText>비밀번호 수정</ClickText>
+          </Click>
         </MiniBlock>
         <MiniBlock>
-            <Click><ClickText>회원 탈퇴하기</ClickText></Click>
+          <Click>
+            <ClickText>회원 탈퇴하기</ClickText>
+          </Click>
         </MiniBlock>
-    </Container>
+      </Container>
     </SafeAreaView>
   );
 }
