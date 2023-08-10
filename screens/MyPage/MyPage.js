fd1731fa69c70042c861d8902b08bc584dfa8e7a
joohee <2170095@ewhain.net>
2023-08-11 07:46:24 +0900

    recoil 형식 수정 & tab 사라지기 구현 & 아이콘 적용

--- screens/MyPage/MyPage.js
+++ screens/MyPage/MyPage.js
@@ -1,20 +1,23 @@
 import React, { useContext, useEffect, useState } from "react";
 import { SafeAreaView } from "react-native";
 import { styled } from "styled-components/native";
-import Records from "../../components/Records";
-import Analysis from "../../components/Analysis";
+import Records from "../../components/myPage/Records";
+import Analysis from "../../components/myPage/Analysis";
 import { colors } from "../../colors";
-import { AppContext } from "../../components/ContextProvider";
 import axios from "axios";
 import { useIsFocused } from "@react-navigation/native";
+import { IsDarkAtom, TabBarAtom } from "../../recoil/MyPageAtom";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { AppContext } from "../../components/ContextProvider";
 
 export default function MyPage() {
-  const isFocused = useIsFocused();
-  const { isDark, setIsTabVisible } = useContext(AppContext);
+  const isFocus = useIsFocused();
+  const isDark = useRecoilValue(IsDarkAtom);
+  const setIsTabVisible = useSetRecoilState(TabBarAtom);
 
   useEffect(() => {
-    isFocused && setIsTabVisible(true);
-  }, [isFocused]);
+    isFocus && setIsTabVisible(true);
+  }, [isFocus]);
 
   const [date, setDate] = useState([]);
 
@@ -41,32 +44,6 @@ export default function MyPage() {
 
   const [showRecords, SetShowRecords] = useState(true);
 
-  const TOTAL_DATA = [
-    {
-      id: 1,
-      date: "2023-07-04",
-      item: [
-        {
-          exercises: [
-            { id: 1, name: "데드리프트", set: 3, weight: 0, dist: 0 },
-            { id: 2, name: "덤벨프레스", set: 3, weight: 4, dist: 0 },
-            { id: 3, name: "바벨 로우", set: 3, weight: 0, dist: 0 },
-            {
-              id: 4,
-              name: "사이드 레터럴 라이즈",
-              set: 10,
-              weight: 0,
-              dist: 0,
-            },
-            { id: 5, name: "레그프레스", set: 3, weight: 50, dist: 0 },
-            { id: 6, name: "크런치", set: 3, weight: 0, dist: 0 },
-          ],
-        },
-      ],
-      calorie: 400,
-    },
-  ];
-
   const Container = styled.ScrollView`
     background-color: ${isDark ? colors.d_background : colors.l_background};
   `;
@@ -85,15 +62,15 @@ export default function MyPage() {
     color: ${colors.grey_3};
   `;
   const ChoiceButton = styled.TouchableOpacity`
-    height: 26px;
     display: block;
+    padding-bottom: 2px;
   `;
   const SelectedTextStyle = {
     color: colors.black,
   };
   const SelectedBoxStyle = {
     borderBottomWidth: 2,
-    border: colors.grey_4,
+    borderColor: colors.l_main,
   };
 
   return (
