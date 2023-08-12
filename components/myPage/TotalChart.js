3372992d65484573c164feb70ae57df42b69007d
joohee <2170095@ewhain.net>
2023-08-13 05:36:43 +0900

    chart -> unit, design, focusPoint

--- components/myPage/TotalChart.js
+++ components/myPage/TotalChart.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Dimensions, Text, View } from "react-native";
 import { LineChart } from "react-native-chart-kit";
-import { Rect, Text as TextSVG, Svg, Line } from "react-native-svg";
+import {
+  Rect,
+  Text as TextSVG,
+  Svg,
+  Line,
+  WithLocalSvg,
+  Circle,
+} from "react-native-svg";
 import { colors } from "../../colors";
 import { styled } from "styled-components/native";
-import WrappedText from "react-native-wrapped-text";
-import { divide } from "react-native-reanimated";
 import { useRecoilValue } from "recoil";
 import { IsDarkAtom } from "../../recoil/MyPageAtom";
 
@@ -29,18 +34,23 @@ export default function TotalChart() {
   });
 
   const chartConfig = {
-    color: () => colors.grey_2,
+    color: () => (isDark ? colors.grey_7 : colors.grey_1),
     labelColor: () => (isDark ? colors.white : colors.black),
     useShadowColorFromDataset: true,
+    decimalPlaces: 0,
     fillShadowGradientFromOpacity: 0.3,
     fillShadowGradientToOpacity: 0,
-
-    backgroundGradientFrom: isDark ? colors.grey_7 : colors.grey_1,
-    backgroundGradientTo: isDark ? colors.grey_7 : colors.grey_1,
+    backgroundGradientFrom: isDark ? "#595F72" : "#F6F8FA",
+    backgroundGradientTo: isDark ? "#595F72" : "#F6F8FA",
     propsForLabels: {
       fontSize: 11,
       fonstWeight: 600,
       lineHeight: 16.5,
+    },
+    propsForBackgroundLines: {
+      strokeWidth: 1,
+      stroke: isDark ? colors.grey_6 : colors.grey_2,
+      strokeDasharray: "0",
     },
     propsForDots: {
       r: "5",
@@ -48,27 +58,15 @@ export default function TotalChart() {
   };
   const screenWidth = Dimensions.get("window").width;
 
-  //dataArray 비율 바꾸고, 범위 비슷하도록... 값/max*1.2정도 // 값 받아올때는 값*max/1.2를 하고.... y값 받아올 수 있으면 message default값으로 넣어서 좋은디....
-  // l_main 쪽은 kcal값을 받아오고.. green 쪽은 km 값을 받아오기!! => 단위도 변경해야함..
-
-  function divideArrayByMax(arr) {
-    if (arr.length === 0) {
-      return [];
-    }
-    const max = Math.max(...arr);
-    const dividedArray = arr.map((value) => value / max);
-    return dividedArray;
-  }
-
-  const KcalData = [4, 3, 7, 6, 2, 5, 6, 9];
+  const KcalData = [200, 500, 600, 900, 400, 300, 700, 1100];
   const KcalMax = Math.max(...KcalData);
-  const KmData = [2, 5, 6, 9, 4, 3, 7, 6];
+  const KmData = [4, 3, 7, 6, 2, 5, 6, 6];
   const KmMax = Math.max(...KmData);
   const calorieData = KcalData.map((value) => value / KcalMax);
   const distanceData = KmData.map((value) => value / KmMax);
-
-  //console.log(calorieData);
-  //console.log(distanceData);
+  const realMax = Math.max(KmMax, KcalMax);
+  const [maxData, setMaxData] = useState();
+  const dataLength = KcalData.length;
 
   const data = {
     labels: ["7월 1주", "2주", "3주", "4주", "8월 1주", "2주", "3주", "4주"],
@@ -84,53 +82,39 @@ export default function TotalChart() {
         strokeWidth: 2,
       },
       {
-        data: [1],
-        color: () => "separent",
+        data: [1.2],
+        color: () => "transparent",
       },
       {
         data: [0],
-        color: () => "separent",
+        color: () => "transparent",
       },
     ],
     //legend: ["kcal", "km"],
   };
 
-  const lastDataIndex = data.labels.length - 1;
-  const valueDataset0 = data.datasets[0].data[lastDataIndex];
-  const valueDataset1 = data.datasets[1].data[lastDataIndex];
-  const maxValue = Math.max(valueDataset0, valueDataset1);
-  const isDataset0Larger = maxValue === valueDataset0;
-
-  /*
-  const getDataPointPositions = () => {
-    return data.datasets[0].data.map((value, index) => ({
-      x: (index * screenWidth) / (data.labels.length - 1),
-      y: 230 - (230 * value) / 25, // Adjust this value based on your chart's data range
-      value: value,
-    }));
-  };
-
-  const dataPointPositions = getDataPointPositions();
-  const lastDataPoint = dataPointPositions[dataPointPositions.length - 1];
- 
-  const [message, setMessage] = useState({
-    x: lastDataPoint.x,
-    y: lastDataPoint.y,
-    visible: true,
-    value: lastDataPoint.value,
-  });
-  */
+  const [unit, setUnit] = useState("kcal");
+  const [render, setRender] = useState(
+    (data.labels.length * screenWidth) / data.labels.length
+  );
 
   return (
     <BoxContainer>
       <Container
         horizontal
         showsHorizontalScrollIndicator={false}
-        contentOffset={{ x: (data.labels.length * screenWidth) / 8, y: 0 }}
+        contentOffset={{
+          x: render,
+          y: 0,
+        }}
       >
         <LineChart
           data={data}
-          width={(data.labels.length * screenWidth) / 4}
+          width={
+            data.labels.length > 3
+              ? (data.labels.length * screenWidth) / 4
+              : screenWidth
+          }
           //width={screenWidth-48}
           height={230}
           chartConfig={chartConfig}
@@ -144,48 +128,73 @@ export default function TotalChart() {
           }}
           decorator={() => {
             return message.visible ? (
-              <View
-                style={{
-                  position: "absolute",
-                  width: 62,
-                  height: 32,
-                  top: message.y - 30,
-                  left: message.x - 36,
-                  backgroundColor: isDark ? colors.white : colors.black,
-                  borderRadius: 12,
-                  alignItems: "center",
-                  justifyContent: "center",
-                  flexDirection: "row",
-                }}
-              >
-                <Text
+              <>
+                <Circle
+                  cx={message.x}
+                  cy={message.y}
+                  r={3}
+                  fill={isDark ? colors.grey_7 : colors.white}
+                />
+                <Line
+                  x1={message.x}
+                  x2={message.x}
+                  y1={message.y + 5}
+                  y2={189}
+                  stroke={isDark ? colors.white : colors.black}
+                  strokeWidth={2}
+                  strokeDasharray={3}
+                ></Line>
+                <View
                   style={{
-                    fontSize: 11,
-                    fontWeight: 700,
-                    color: isDark ? colors.black : colors.white,
-                    textAlign: "center",
+                    position: "absolute",
+                    width: 62,
+                    height: 32,
+                    top: message.y - 40,
+                    left: message.x === 0 ? 2 : message.x - 31,
+                    backgroundColor: isDark ? colors.white : colors.black,
+                    borderRadius: 12,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "row",
                   }}
                 >
-                  {message.value * KcalMax}
-                </Text>
-                {
                   <Text
                     style={{
                       fontSize: 11,
-                      fontWeight: 500,
+                      fontWeight: 700,
                       color: isDark ? colors.black : colors.white,
                       textAlign: "center",
                     }}
                   >
-                    {" kcal"}
+                    {message.value * maxData}
                   </Text>
-                }
-              </View>
+                  {
+                    <Text
+                      style={{
+                        fontSize: 11,
+                        fontWeight: 500,
+                        color: isDark ? colors.black : colors.white,
+                        textAlign: "center",
+                      }}
+                    >
+                      {unit}
+                    </Text>
+                  }
+                </View>
+              </>
             ) : null;
           }}
           onDataPointClick={(data) => {
+            data.dataset.data === calorieData
+              ? setUnit(" kcal")
+              : setUnit(" km");
+            data.dataset.data === calorieData
+              ? setMaxData(KcalMax)
+              : setMaxData(KmMax);
+            data.dataset.data.length > 3
+              ? setRender(data.x - screenWidth + 94)
+              : "";
             let isSamePoint = message.x === data.x && message.y === data.y;
-
             isSamePoint
               ? setMessage((previousState) => {
                   return {
