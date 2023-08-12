770d834dc74e0e0d102df4aa5e8ebc676bc3c74f
NeoSelf1 <neoself1105@gmail.com>
2023-08-13 04:09:30 +0900

    bugOccured

--- components/Shared/MyRoutine_Shared.js
+++ components/Shared/MyRoutine_Shared.js
@@ -5,42 +5,49 @@ import { WithLocalSvg } from "react-native-svg";
 import Edit from "../../assets/SVGs/Edit.svg";
 import Left from "../../assets/SVGs/Left.svg";
 
-const HeaderContainer = styled.View`
-  width: ${ScreenWidth}px;
-  background-color: white;
-  height: 56px;
-  align-items: center;
-  justify-content: center;
-`;
-const Title = styled.Text`
-  font-size: 17px;
-  font-weight: 600;
-  color: ${colors.black};
-`;
-
-const ButtonText = styled.Text`
-  font-size: 17px;
-  font-weight: 600;
-  color: ${colors.l_main};
-`;
 const Button = styled.TouchableOpacity`
   position: absolute;
   justify-content: center;
   align-items: center;
-
   height: 56px;
   width: 56px;
 `;
 
-export const Header = ({ mode, parentFunction, onPress }) => {
+export const Header = ({ mode, onPress, toggleMode, isDark }) => {
+  const HeaderContainer = styled.View`
+    width: ${ScreenWidth}px;
+    background-color: ${isDark
+      ? mode
+        ? colors.grey_9
+        : colors.black
+      : colors.white};
+    height: 56px;
+    align-items: center;
+    justify-content: center;
+  `;
+  const Title = styled.Text`
+    font-size: 17px;
+    font-weight: 600;
+    color: ${isDark ? colors.white : colors.black};
+  `;
+  const ButtonText = styled.Text`
+    font-size: 17px;
+    font-weight: 600;
+    color: ${colors.l_main};
+  `;
   const handleSubmit = () => {
-    parentFunction();
+    toggleMode();
   };
   return (
     <HeaderContainer>
       {mode && (
         <Button style={{ left: 12 }} onPress={onPress}>
-          <WithLocalSvg width={24} height={24} asset={Left} />
+          <WithLocalSvg
+            width={24}
+            height={24}
+            asset={Left}
+            color={isDark ? colors.white : colors.black}
+          />
         </Button>
       )}
       <Title>{mode ? "루틴커스텀" : "마이루틴"}</Title>
@@ -48,7 +55,12 @@ export const Header = ({ mode, parentFunction, onPress }) => {
         {mode ? (
           <ButtonText>완료</ButtonText>
         ) : (
-          <WithLocalSvg width={24} height={24} asset={Edit} />
+          <WithLocalSvg
+            width={24}
+            height={24}
+            asset={Edit}
+            color={isDark ? colors.white : colors.black}
+          />
         )}
       </Button>
     </HeaderContainer>
@@ -85,29 +97,35 @@ export function listToObject(list) {
 
   return object;
 }
-export const ComponentTitle = ({ title, subTitle }) => {
-  const TextContainer = styled.View`
-    flex-direction: row;
-    margin-top: 40px;
-    width: 100%;
-    justify-content: space-between;
-  `;
-  const _ComponentTitle = styled.Text`
-    font-size: 17px;
-    font-weight: 600;
-  `;
-  const _ComponentSubTitle = styled.Text`
-    font-size: 13px;
-    flex: 1;
-    color: ${colors.grey_7};
-    margin-top: 4px;
-    margin-left: 8px;
-    font-weight: 300;
-  `;
+const TextContainer = styled.View`
+  flex-direction: row;
+  margin-top: 40px;
+  width: 100%;
+  justify-content: space-between;
+`;
+const _ComponentTitle = styled.Text`
+  font-size: 17px;
+  font-weight: 600;
+`;
+const _ComponentSubTitle = styled.Text`
+  font-size: 13px;
+  flex: 1;
+  color: ${colors.grey_7};
+  margin-top: 4px;
+  margin-left: 8px;
+  font-weight: 300;
+`;
+export const ComponentTitle = ({ title, subTitle, isDark }) => {
   return (
     <TextContainer>
-      <_ComponentTitle>{title}</_ComponentTitle>
-      <_ComponentSubTitle>{subTitle}</_ComponentSubTitle>
+      <_ComponentTitle style={{ color: isDark ? colors.white : colors.black }}>
+        {title}
+      </_ComponentTitle>
+      <_ComponentSubTitle
+        style={{ color: isDark ? colors.grey_3 : colors.black }}
+      >
+        {subTitle}
+      </_ComponentSubTitle>
     </TextContainer>
   );
 };
