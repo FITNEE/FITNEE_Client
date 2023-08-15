697edb89d7bba56a046aaa8fa83ee5d61d148a58
NeoSelf1 <neoself1105@gmail.com>
2023-08-15 17:52:01 +0900

    AppBaseAdded

--- AppBase.js
+++ AppBase.js
@@ -1,12 +1,10 @@
 /* eslint-disable */
 import AppLoading from "expo-app-loading";
-
 import OnBoardingNav from "./navigators/OnBoardingNav";
 import LoggedInNav from "./navigators/LoggedInNav";
 import { useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { loggedInState } from "./recoil/AuthAtom";
 
 export default function AppBase() {
@@ -14,15 +12,6 @@ export default function AppBase() {
   const [loggedIn, setLoggedIn] = useRecoilState(loggedInState);
 
   const onFinish = () => setLoading(false);
-
-  // const toggleLogin = () => {
-  //   if (loggedIn) {
-  //     AsyncStorage.removeItem("userId");
-  //     setLoggedIn(false);
-  //   } else {
-  //     setLoggedIn(true);
-  //   }
-  // };
 
   const preload = async () => {
     await AsyncStorage.getItem("userId").then((userId) => {
