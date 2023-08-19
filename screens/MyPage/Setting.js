43d7c398964ae8cd6505f26806e6fdd674f7c15d
joohee <2170095@ewhain.net>
2023-08-20 04:55:59 +0900

    mypage 아이콘 추가

--- screens/MyPage/Setting.js
+++ screens/MyPage/Setting.js
@@ -15,6 +15,8 @@ import { loggedInState } from "../../recoil/AuthAtom";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Alert } from "react-native";
 import CustomSwitch from "../../components/myPage/CustomSwitch";
+import Profile_man from "../../assets/SVGs/Profile_man.svg";
+import Profile_woman from "../../assets/SVGs/Profile_woman.svg";
 
 const Profile = styled.TouchableOpacity`
   width: 100%;
@@ -133,9 +135,21 @@ export default function Setting({ navigation }) {
           }}
         >
           <ProfileInfo>
-            <ProfileImage
-              style={{ backgroundColor: getGender == 1 ? "blue" : "pink" }}
-            />
+            {getGender == 1 ? (
+              <Profile_man
+                style={{ marginRight: 8 }}
+                width={40}
+                height={40}
+                color={isDark ? colors.grey_7 : colors.grey_2}
+              />
+            ) : (
+              <Profile_woman
+                style={{ marginRight: 8 }}
+                width={40}
+                height={40}
+                color={isDark ? colors.grey_7 : colors.grey_2}
+              />
+            )}
             <ProfileContents>
               <Name isDark={isDark}>{getUserName}</Name>
             </ProfileContents>
