de95e3125089b34396c08ca280c29fd30bfaca22
NeoSelf1 <neoself1105@gmail.com>
2023-08-19 19:22:48 +0900

    androidAdjustDone

--- components/myRoutine/WeekCalendar.js
+++ components/myRoutine/WeekCalendar.js
@@ -1,93 +1,84 @@
-import React, { Component } from "react";
-import { days } from "./data";
-import { colors } from "../../colors";
-import Exercise from "../../assets/SVGs/Exercise.svg";
-import { TextContainer, DayText } from "../../components/ScheduleChanger";
-import { styled } from "styled-components/native";
+import React, { Component } from 'react'
+import { days } from './data'
+import { colors } from '../../colors'
+import Exercise from '../../assets/SVGs/Exercise.svg'
+import { TextContainer, DayText } from '../../components/ScheduleChanger'
+import { styled } from 'styled-components/native'
 
 const DayContainer = styled.TouchableOpacity`
-  width: 35px;
-  height: 60px;
-  border-radius: 30px;
-  padding-top: 8px;
-  justify-content: space-between;
-  align-items: center;
-`;
+    width: 35px;
+    height: 60px;
+    border-radius: 30px;
+    padding-top: 8px;
+    justify-content: space-between;
+    align-items: center;
+`
 const Circle = styled.View`
-  width: 24px;
-  height: 24px;
-  margin-bottom: 5.5px;
-  border-radius: 12px;
-`;
+    width: 24px;
+    height: 24px;
+    margin-bottom: 5.5px;
+    border-radius: 12px;
+`
 const ScheduleContainer = styled.View`
-  flex-direction: row;
-  padding: 24px;
-  width: 100%;
-  justify-content: space-between;
-  height: 100px;
-  align-items: center;
-`;
+    flex-direction: row;
+    padding: 24px;
+    width: 100%;
+    justify-content: space-between;
+    height: 100px;
+    align-items: center;
+    border-bottom-width: 1px;
+`
 
-export default WeekCalendar = ({
-  setSelectedDay,
-  selectedDay,
-  SCHEDULE,
-  isDark,
-}) => {
-  return (
-    <ScheduleContainer
-      style={{ backgroundColor: isDark ? colors.grey_9 : colors.white }}
-    >
-      <TextContainer>
-        {days.map((item, id) => (
-          <DayContainer
-            onPress={() => setSelectedDay(id)}
-            key={id}
-            style={
-              id == selectedDay && [
-                {
-                  backgroundColor: isDark ? colors.d_sub_1 : colors.l_main,
-                },
-                isDark && { borderColor: colors.d_main, borderWidth: 1 },
-              ]
-            }
-          >
-            <DayText
-              style={
-                id == selectedDay
-                  ? { color: isDark ? colors.d_main : colors.white }
-                  : { color: isDark ? colors.white : colors.black }
-              }
-            >
-              {item}
-            </DayText>
-            {SCHEDULE[id]?.routineId != 0 && // SCHEDULE 배열의 id번째 요소는 0이 아닌 다른 숫자라면 id번째 요일에 운동이 존재한다는 것 의미
-              SCHEDULE[id] && ( // SCHEDULE 배열의 id번째 요소가 존재하지 않는다는것은, 애초에 SCHEDULE배열로 후가공하기 위해 필요한 rawData가 백엔드로부터 전해지지 않았다는 것을 의미
-                <Circle
-                  style={
-                    id == selectedDay //만일 선택된 날짜랑 운동있는 요일이랑 겹치면 색상을 변경해야하기 때문에 css 예외처리
-                      ? {
-                          backgroundColor: isDark
-                            ? colors.d_main
-                            : colors.white,
+export default WeekCalendar = ({ setSelectedDay, selectedDay, SCHEDULE, isDark }) => {
+    return (
+        <ScheduleContainer
+            style={{
+                backgroundColor: isDark ? colors.grey_9 : colors.white,
+                borderBottomColor: isDark ? colors.grey_8 : colors.grey_2,
+            }}
+        >
+            <TextContainer>
+                {days.map((item, id) => (
+                    <DayContainer
+                        onPress={() => setSelectedDay(id)}
+                        key={id}
+                        style={
+                            id == selectedDay && [
+                                {
+                                    backgroundColor: isDark ? colors.d_sub_1 : colors.l_main,
+                                },
+                                isDark && { borderColor: colors.d_main, borderWidth: 1 },
+                            ]
                         }
-                      : {
-                          backgroundColor: isDark
-                            ? colors.d_main
-                            : colors.grey_3,
-                        }
-                  }
-                >
-                  <Exercise
-                    width={24}
-                    height={24}
-                    color={isDark ? colors.white : colors.l_main}
-                  />
-                </Circle>
-              )}
-          </DayContainer>
-        ))}
-      </TextContainer>
-    </ScheduleContainer>
-  );
-};
+                    >
+                        <DayText
+                            style={
+                                id == selectedDay
+                                    ? { color: isDark ? colors.d_main : colors.white }
+                                    : { color: isDark ? colors.white : colors.black }
+                            }
+                        >
+                            {item}
+                        </DayText>
+                        {SCHEDULE[id]?.routineId != 0 && // SCHEDULE 배열의 id번째 요소는 0이 아닌 다른 숫자라면 id번째 요일에 운동이 존재한다는 것 의미
+                            SCHEDULE[id] && ( // SCHEDULE 배열의 id번째 요소가 존재하지 않는다는것은, 애초에 SCHEDULE배열로 후가공하기 위해 필요한 rawData가 백엔드로부터 전해지지 않았다는 것을 의미
+                                <Circle
+                                    style={
+                                        id == selectedDay //만일 선택된 날짜랑 운동있는 요일이랑 겹치면 색상을 변경해야하기 때문에 css 예외처리
+                                            ? {
+                                                  backgroundColor: isDark ? colors.d_main : colors.white,
+                                              }
+                                            : {
+                                                  backgroundColor: isDark ? colors.d_main : colors.grey_3,
+                                              }
+                                    }
+                                >
+                                    <Exercise width={24} height={24} color={isDark ? colors.white : colors.l_main} />
+                                </Circle>
+                            )}
+                    </DayContainer>
+                ))}
+            </TextContainer>
+        </ScheduleContainer>
+    )
+}
--- screens/OnBoarding/Login.js
+++ screens/OnBoarding/Login.js
@@ -1,113 +1,107 @@
-import React, { useContext, useState } from "react";
-import styled from "styled-components/native";
-import { colors } from "../../colors";
-import { Button, BackButton } from "../../Shared";
-import {
-  Input,
-  ScreenLayout,
-  StatusText,
-  SubText,
-  Title,
-} from "../../components/Shared/OnBoarding_Shared";
-import axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { IsDarkAtom } from "../../recoil/MyPageAtom";
-import { loggedInState } from "../../recoil/AuthAtom";
+import React, { useContext, useState } from 'react'
+import styled from 'styled-components/native'
+import { colors } from '../../colors'
+import { Button, BackButton } from '../../Shared'
+import { Input, ScreenKeyboardLayout, StatusText, SubText, Title } from '../../components/Shared/OnBoarding_Shared'
+import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { IsDarkAtom } from '../../recoil/MyPageAtom'
+import { loggedInState } from '../../recoil/AuthAtom'
 
 const TextContainer = styled.View`
-  margin-top: 124px;
-  flex-direction: column;
-  align-items: flex-start;
-  width: 100%;
-  justify-content: center;
-`;
+    margin-top: 124px;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+    justify-content: center;
+`
 const InputContainer = styled.View`
-  width: 100%;
-  flex: 1;
-  margin-top: 54px;
-`;
+    width: 100%;
+    flex: 1;
+    margin-top: 54px;
+`
 
 const Login = ({ route, navigation }) => {
-  const isDark = useRecoilValue(IsDarkAtom);
-  const [loggedIn, setLoggedIn] = useRecoilState(loggedInState);
+    const isDark = useRecoilValue(IsDarkAtom)
+    const [loggedIn, setLoggedIn] = useRecoilState(loggedInState)
 
-  const [PW, setPW] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [statusText, setStatusText] = useState("");
+    const [PW, setPW] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const [statusText, setStatusText] = useState('')
 
-  const email = route.params.email;
-  const postLogin = async (email, PW) => {
-    try {
-      let url = "https://gpthealth.shop/";
-      let detailAPI = "app/user/login";
-      console.log(email, PW);
-      let data = { userId: email, userPw: PW };
-      const response = await axios.post(url + detailAPI, data, {
-        headers: {
-          "Content-Type": `application/json`,
-        },
-      });
-      const result = response.data;
-      return result;
-    } catch (error) {
-      console.error("Failed to fetch data:", error);
+    const email = route.params.email
+    const postLogin = async (email, PW) => {
+        try {
+            let url = 'https://gpthealth.shop/'
+            let detailAPI = 'app/user/login'
+            console.log(email, PW)
+            let data = { userId: email, userPw: PW }
+            const response = await axios.post(url + detailAPI, data, {
+                headers: {
+                    'Content-Type': `application/json`,
+                },
+            })
+            const result = response.data
+            return result
+        } catch (error) {
+            console.error('Failed to fetch data:', error)
+        }
     }
-  };
-  const handlePress = () => {
-    setIsLoading(true);
-    postLogin(email, PW).then((response) => {
-      console.log(response);
-      //로그인 성공 시,
-      if (response.code == 1000) {
-        AsyncStorage.setItem("accessToken", response.result.accessToken).then(
-          console.log("accessToken set to AsyncStorage")
-        );
-        setLoggedIn(true);
-      } else {
-        setStatusText(response.message);
-        setIsLoading(false);
-      }
-    });
-  };
+    const handlePress = () => {
+        setIsLoading(true)
+        postLogin(email, PW).then((response) => {
+            console.log(response)
+            //로그인 성공 시,
+            if (response.code == 1000) {
+                AsyncStorage.setItem('accessToken', response.result.accessToken).then(
+                    console.log('accessToken set to AsyncStorage'),
+                )
+                setLoggedIn(true)
+            } else {
+                setStatusText(response.message)
+                setIsLoading(false)
+            }
+        })
+    }
 
-  return (
-    <ScreenLayout isRelative={false} isDark={isDark}>
-      <BackButton isDark={isDark} onPress={() => navigation.goBack()} />
-      <TextContainer>
-        <Title text="다시 만나 반가워요!" isDark={isDark} />
-        <SubText text="비밀번호를 입력해주세요." isDark={isDark} />
-      </TextContainer>
-      <InputContainer>
-        <StatusText style={{ color: colors.red }}>{statusText}</StatusText>
-        <Input
-          style={[
-            statusText && { borderWidth: 1, borderColor: colors.red },
-            {
-              backgroundColor: isDark ? colors.black : colors.white,
-              color: isDark ? colors.white : colors.black,
-            },
-          ]}
-          autoCapitalize="none"
-          placeholderTextColor={colors.grey_5}
-          autoFocus
-          onSubmitEditing={() => handlePress()}
-          placeholder="password"
-          secureTextEntry={true}
-          returnKeyType="done"
-          blurOnSubmit={false}
-          onChangeText={(text) => setPW(text)}
-        />
-      </InputContainer>
-      <Button
-        isDark={isDark}
-        loading={isLoading}
-        enabled={(PW.length >= 4) & !isLoading}
-        text="로그인"
-        onPress={() => handlePress()}
-      />
-    </ScreenLayout>
-  );
-};
+    return (
+        <ScreenKeyboardLayout isRelative={false} isDark={isDark}>
+            <BackButton isDark={isDark} onPress={() => navigation.goBack()} />
+            <TextContainer>
+                <Title text="다시 만나 반가워요!" isDark={isDark} />
+                <SubText text="비밀번호를 입력해주세요." isDark={isDark} />
+            </TextContainer>
+            <InputContainer>
+                <StatusText style={{ color: colors.red }}>{statusText}</StatusText>
+                <Input
+                    style={[
+                        statusText && { borderWidth: 1, borderColor: colors.red },
+                        {
+                            backgroundColor: isDark ? colors.black : colors.white,
+                            color: isDark ? colors.white : colors.black,
+                        },
+                    ]}
+                    autoCapitalize="none"
+                    placeholderTextColor={colors.grey_5}
+                    autoFocus
+                    onSubmitEditing={() => handlePress()}
+                    placeholder="password"
+                    secureTextEntry={true}
+                    returnKeyType="done"
+                    blurOnSubmit={false}
+                    onChangeText={(text) => setPW(text)}
+                />
+            </InputContainer>
+            <Button
+                isDark={isDark}
+                loading={isLoading}
+                enabled={(PW.length >= 4) & !isLoading}
+                text="로그인"
+                onPress={() => handlePress()}
+            />
+        </ScreenKeyboardLayout>
+    )
+}
 
-export default Login;
+export default Login
