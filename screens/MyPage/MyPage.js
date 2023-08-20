7ffd920b763ceb420d494de0b2a1f765200e5bbd
Choi Seho <kindseho@naver.com>
2023-08-20 13:26:24 +0900

    merge: 세호 머지

--- screens/MyPage/MyPage.js
+++ screens/MyPage/MyPage.js
@@ -28,8 +28,8 @@ const Choice = styled.View`
     gap: 16px;
 `
 const ChoiceButton = styled.TouchableOpacity`
-  padding-bottom: 2px;
-`;
+    padding-bottom: 2px;
+`
 
 export default function MyPage() {
     const isFocus = useIsFocused()
@@ -38,90 +38,90 @@ export default function MyPage() {
 
     useEffect(() => {
         isFocus && setIsTabVisible(true)
-    }, [isFocus]);
+    }, [isFocus])
 
-  const getDayHealth = async (checkedDate) => {
-    try {
-      let url = "https://gpthealth.shop/";
-      let detailAPI = `app/mypage/exercise?date=${checkedDate}`;
-      const response = await axios.get(url + detailAPI);
-      const checkResult = response.data;
-      return checkResult;
-    } catch (error) {
-      console.error("Failed to fetch data:", error);
-      
-    }
-
-    const getMyPageData = async (month) => {
+    const getDayHealth = async (checkedDate) => {
         try {
             let url = 'https://gpthealth.shop/'
-            let detailAPI = `app/mypage?month=${month}`
+            let detailAPI = `app/mypage/exercise?date=${checkedDate}`
             const response = await axios.get(url + detailAPI)
-            const dateResult = response.data
-            return dateResult
+            const checkResult = response.data
+            return checkResult
         } catch (error) {
             console.error('Failed to fetch data:', error)
         }
-    }
 
-    const [weekData, setWeekData] = useState()
-
-    const getWeekHealth = async () => {
-        try {
-            let url = 'https://gpthealth.shop/'
-            let detailAPI = `app/mypage/record`
-            const response = await axios.get(url + detailAPI)
-            const weekResult = response.data
-            return weekResult
-        } catch (error) {
-            console.error('Failed to fetch data:', error)
+        const getMyPageData = async (month) => {
+            try {
+                let url = 'https://gpthealth.shop/'
+                let detailAPI = `app/mypage?month=${month}`
+                const response = await axios.get(url + detailAPI)
+                const dateResult = response.data
+                return dateResult
+            } catch (error) {
+                console.error('Failed to fetch data:', error)
+            }
         }
+
+        const [weekData, setWeekData] = useState()
+
+        const getWeekHealth = async () => {
+            try {
+                let url = 'https://gpthealth.shop/'
+                let detailAPI = `app/mypage/record`
+                const response = await axios.get(url + detailAPI)
+                const weekResult = response.data
+                return weekResult
+            } catch (error) {
+                console.error('Failed to fetch data:', error)
+            }
+        }
+
+        useEffect(() => {
+            getWeekHealth().then((weekResult) => {
+                setWeekData(weekResult.result)
+            })
+        }, [])
+
+        const [showRecords, SetShowRecords] = useState(true)
+
+        const SelectedTextStyle = {
+            color: isDark ? colors.white : colors.black,
+        }
+        const SelectedBoxStyle = {
+            borderBottomWidth: 2,
+            borderColor: isDark ? colors.d_main : colors.l_main,
+        }
+
+        return (
+            <ScreenLayout isDark={isDark} darkBack={colors.grey_9} lightBack={colors.grey_1}>
+                <Container isDark={isDark}>
+                    <Choice>
+                        <ChoiceButton
+                            onPress={() => {
+                                SetShowRecords(true)
+                            }}
+                            style={showRecords && SelectedBoxStyle}
+                        >
+                            <ChoiceText isDark={isDark} style={showRecords && SelectedTextStyle}>
+                                운동 기록
+                            </ChoiceText>
+                        </ChoiceButton>
+                        <ChoiceButton
+                            onPress={() => {
+                                SetShowRecords(false)
+                            }}
+                            style={!showRecords && SelectedBoxStyle}
+                        >
+                            <ChoiceText isDark={isDark} style={!showRecords && SelectedTextStyle}>
+                                운동 분석 및 현황
+                            </ChoiceText>
+                        </ChoiceButton>
+                    </Choice>
+                    {showRecords && <Records getDayHealth={getDayHealth} getMyPageData={getMyPageData} />}
+                    {!showRecords && <Analysis weekData={weekData} />}
+                </Container>
+            </ScreenLayout>
+        )
     }
-
-    useEffect(() => {
-        getWeekHealth().then((weekResult) => {
-            setWeekData(weekResult.result)
-        })
-    }, [])
-
-    const [showRecords, SetShowRecords] = useState(true)
-
-    const SelectedTextStyle = {
-        color: isDark ? colors.white : colors.black,
-    }
-    const SelectedBoxStyle = {
-        borderBottomWidth: 2,
-        borderColor: isDark ? colors.d_main : colors.l_main,
-    }
-
-    return (
-        <ScreenLayout isDark={isDark} darkBack={colors.grey_9} lightBack={colors.grey_1}>
-            <Container isDark={isDark}>
-                <Choice>
-                    <ChoiceButton
-                        onPress={() => {
-                            SetShowRecords(true)
-                        }}
-                        style={showRecords && SelectedBoxStyle}
-                    >
-                        <ChoiceText isDark={isDark} style={showRecords && SelectedTextStyle}>
-                            운동 기록
-                        </ChoiceText>
-                    </ChoiceButton>
-                    <ChoiceButton
-                        onPress={() => {
-                            SetShowRecords(false)
-                        }}
-                        style={!showRecords && SelectedBoxStyle}
-                    >
-                        <ChoiceText isDark={isDark} style={!showRecords && SelectedTextStyle}>
-                            운동 분석 및 현황
-                        </ChoiceText>
-                    </ChoiceButton>
-                </Choice>
-                {showRecords && <Records getDayHealth={getDayHealth} getMyPageData={getMyPageData} />}
-                {!showRecords && <Analysis weekData={weekData} />}
-            </Container>
-        </ScreenLayout>
-    )
 }
