14a77e77bf9d0386e0a850e35e5230c24f078006
leejin_rho <tbdpapdl@gmail.com>
2023-08-22 01:31:41 +0900

    버튼 disabled

--- screens/ExerciseCourse/ExerciseCourse2_2.js
+++ screens/ExerciseCourse/ExerciseCourse2_2.js
@@ -1,36 +1,20 @@
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
-import {
-  SafeAreaView,
-  View,
-  Text,
-  TouchableOpacity,
-  Alert,
-} from "react-native";
-import { TextInput, Dimensions, Animated, StyleSheet } from "react-native";
-const { width, height } = Dimensions.get("window");
-import styled from "styled-components/native";
-import ExerciseCard from "../../components/exerciseCourse/ExerciseCard";
-import { colors } from "../../colors";
-import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
-import NextSet from "../../components/exerciseCourse/NextSet";
-import {
-  BottomSheetModal,
-  BottomSheetModalProvider,
-  BottomSheetBackdrop,
-} from "@gorhom/bottom-sheet";
-import { useRoute, StackActions } from "@react-navigation/native";
-import { useRecoilValue } from "recoil";
-import { IsDarkAtom } from "../../recoil/MyPageAtom";
-import ArrowCircle from "../../assets/SVGs/ArrowCircle.svg";
-import Close from "../../assets/SVGs/Close.svg";
-import ExerciseButton from "../../components/exerciseCourse/ExerciseButton";
-import { useFocusEffect } from "@react-navigation/native";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { SafeAreaView, View, Text, TouchableOpacity, Alert } from 'react-native'
+import { TextInput, Dimensions, Animated, StyleSheet } from 'react-native'
+const { width, height } = Dimensions.get('window')
+import styled from 'styled-components/native'
+import ExerciseCard from '../../components/exerciseCourse/ExerciseCard'
+import { colors } from '../../colors'
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
+import NextSet from '../../components/exerciseCourse/NextSet'
+import { BottomSheetModal, BottomSheetModalProvider, BottomSheetBackdrop } from '@gorhom/bottom-sheet'
+import { useRoute, StackActions } from '@react-navigation/native'
+import { useRecoilValue } from 'recoil'
+import { IsDarkAtom } from '../../recoil/MyPageAtom'
+import ArrowCircle from '../../assets/SVGs/ArrowCircle.svg'
+import Close from '../../assets/SVGs/Close.svg'
+import ExerciseButton from '../../components/exerciseCourse/ExerciseButton'
+import { useFocusEffect } from '@react-navigation/native'
 
 const StartButton = styled.TouchableOpacity`
   padding: 8px 12px;
@@ -42,7 +26,7 @@ const StartButton = styled.TouchableOpacity`
   background: ${colors.l_main};
   width: 99px;
   margin-right: 24px;
-`;
+`
 
 const StartButtonText = styled.Text`
   color: ${colors.white};
@@ -50,7 +34,7 @@ const StartButtonText = styled.Text`
   font-size: 13px;
   font-family: Pretendard-SemiBold;
   line-height: 19.5px;
-`;
+`
 
 const ExerciseCircle = styled.View`
   width: 307px;
@@ -60,13 +44,13 @@ const ExerciseCircle = styled.View`
   margin-bottom: 70px;
   justify-content: center;
   align-items: center;
-`;
+`
 
 const TimerText = styled.Text`
   color: ${({ isDark }) => (isDark ? colors.white : colors.black)};
   font-size: 56px;
   font-family: Pretendard-Regular;
-`;
+`
 
 const StopExercise = styled.TouchableOpacity`
   width: 24px;
@@ -74,11 +58,11 @@ const StopExercise = styled.TouchableOpacity`
   position: absolute;
   top: 20px;
   right: 24px;
-`;
+`
 
 const BlankBox = styled.View`
   height: 131px;
-`;
+`
 
 export default function ExerciseCourse_2_2({
   navigation,
@@ -90,63 +74,51 @@ export default function ExerciseCourse_2_2({
   toggleShowExerciseCourse2_2,
 }) {
   const goToStartExercise = () => {
-    navigation.navigate("StartExercise");
-  };
+    navigation.navigate('StartExercise')
+  }
 
   const goToNextExercise = () => {
-    setIsPlaying(false);
+    setIsPlaying(false)
     navigation.dispatch(
-      StackActions.replace("ExerciseCourse", {
+      StackActions.replace('ExerciseCourse', {
         dataList: dataList,
         listIndex: listIndex + 1,
         totalTime: totalTime - restTime,
         routineIdx: routineIdx,
-      })
-    );
-  };
+      }),
+    )
+  }
 
   const OpenConfirm = () => {
-    Alert.alert(
-      "현재 진행중인 운동루틴을 중단하시겠습니까?",
-      "현재까지 운동하신 내용은 저장되지 않습니다.",
-      [
-        { text: "취소", onPress: () => console.log("Cancel Stop") },
-        {
-          text: "운동 중단하기",
-          onPress: goToStartExercise,
-          style: "destructive",
-        },
-      ]
-    );
-  };
+    Alert.alert('현재 진행중인 운동루틴을 중단하시겠습니까?', '현재까지 운동하신 내용은 저장되지 않습니다.', [
+      { text: '취소', onPress: () => console.log('Cancel Stop') },
+      {
+        text: '운동 중단하기',
+        onPress: goToStartExercise,
+        style: 'destructive',
+      },
+    ])
+  }
 
-  const [isPlaying, setIsPlaying] = React.useState(true);
-  const [duration, setDuration] = React.useState(30);
-  const [isLast, setIsLast] = useState(false);
+  const [isPlaying, setIsPlaying] = React.useState(true)
+  const [duration, setDuration] = React.useState(30)
 
-  // //data route
-  // const route = useRoute();
-  // const dataList = route.params.dataList;
-  // const listIndex = route.params.listIndex;
-  // const routineIdx = route.params.routineIdx;
-  // const totalTime = route.params.totalTime;
-
-  const [restTime, setRestTime] = useState(0);
+  const [restTime, setRestTime] = useState(0)
 
   const children = ({ remainingTime }) => {
-    const minutes = Math.floor(remainingTime / 60);
-    min = minutes < 10 ? "0" + minutes : minutes;
-    const seconds = remainingTime % 60;
-    sec = seconds < 10 ? "0" + seconds : seconds;
+    const minutes = Math.floor(remainingTime / 60)
+    min = minutes < 10 ? '0' + minutes : minutes
+    const seconds = remainingTime % 60
+    sec = seconds < 10 ? '0' + seconds : seconds
 
-    return `${min}:${sec}`;
-  };
+    return `${min}:${sec}`
+  }
 
   //   const RestTime = ({ remainingTime }) => {
   //     return setRestTime(30 - remainingTime);
   //   };
 
-  const isDark = useRecoilValue(IsDarkAtom);
+  const isDark = useRecoilValue(IsDarkAtom)
 
   return (
     <SafeAreaView
@@ -157,11 +129,7 @@ export default function ExerciseCourse_2_2({
     >
       <ExerciseCard isDark={isDark} exerciseName="휴식 시간">
         <StopExercise onPress={() => OpenConfirm()}>
-          <Close
-            width={24}
-            height={24}
-            color={isDark ? colors.white : colors.black}
-          />
+          <Close width={24} height={24} color={isDark ? colors.white : colors.black} />
         </StopExercise>
         <ExerciseCircle isDark={isDark}>
           <CountdownCircleTimer
@@ -173,29 +141,21 @@ export default function ExerciseCourse_2_2({
             trailColor={isDark ? colors.grey_7 : colors.grey_3}
             onComplete={toggleShowExerciseCourse2_2}
             updateInterval={0.001}
-            rotation={"counterclockwise"}
+            rotation={'counterclockwise'}
           >
             {({ remainingTime }) => (
               //RestTime({ remainingTime }),
-              <TimerText isDark={isDark}>
-                {children({ remainingTime })}
-              </TimerText>
+              <TimerText isDark={isDark}>{children({ remainingTime })}</TimerText>
             )}
           </CountdownCircleTimer>
         </ExerciseCircle>
 
         <NextSet
-          set="1"
-          kg={dataList[listIndex].sets[0].weight}
-          num={dataList[listIndex].sets[0].rep}
+          set={setId + 1}
+          kg={dataList[listIndex].sets[setId].weight}
+          num={dataList[listIndex].sets[setId].rep}
           isDark={isDark}
         />
-        {/* <NextSet
-          set="2"
-          kg={dataList[listIndex + 1].sets[1].weight}
-          num={dataList[listIndex + 1].sets[1].rep}
-          isDark={isDark}
-        /> */}
         <BlankBox />
 
         <ExerciseButton //운동 시작 버튼
@@ -207,5 +167,5 @@ export default function ExerciseCourse_2_2({
         />
       </ExerciseCard>
     </SafeAreaView>
-  );
+  )
 }
--- screens/ExerciseCourse/StartExercise.js
+++ screens/ExerciseCourse/StartExercise.js
@@ -16,268 +16,267 @@ import Loading from '../../components/exerciseCourse/Loading'
 import NoRoutine from '../../components/exerciseCourse/NoRoutine'
 
 const RecTextLine = styled.View`
-    flex-direction: row;
-    width: 279px;
-    margin-bottom: 4px;
+  flex-direction: row;
+  width: 279px;
+  margin-bottom: 4px;
 `
 
 const CirclesLine = styled.View`
-    flex-direction: row;
-    width: 256px;
-    justify-content: space-around;
+  flex-direction: row;
+  width: 256px;
+  justify-content: space-around;
 `
 
 const Container = styled.View`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
-    padding: 0px 23.5px;
-    background: ${({ isDark }) => (isDark ? colors.d_background : colors.white)};
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 23.5px;
+  background: ${({ isDark }) => (isDark ? colors.d_background : colors.white)};
 `
 
 const ExerciseButton = styled.TouchableOpacity`
-    width: 111px;
-    height: 111px;
-    flex-shrink: 0;
-    border-radius: 55.5px;
-    background: ${({ isDark }) => (isDark ? colors.d_main : colors.l_main)};
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  width: 111px;
+  height: 111px;
+  flex-shrink: 0;
+  border-radius: 60px;
+  background: ${({ isDark }) => (isDark ? colors.d_main : colors.l_main)};
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 const ExerciseText = styled.Text`
-    font-family: Pretendard-SemiBold;
-    font-size: 24px;
-    text-align: center;
-    line-height: 33.6px;
-    color: ${({ isDark }) => (isDark ? colors.white : colors.black)};
+  font-family: Pretendard-SemiBold;
+  font-size: 24px;
+  text-align: center;
+  line-height: 33.6px;
+  color: ${({ isDark }) => (isDark ? colors.white : colors.black)};
 `
 
 const ExerciseRec = styled.View`
-    width: 311px;
-    height: 175px;
-    border-radius: 12px;
-    background: ${({ isDark }) => (isDark ? colors.grey_8 : colors.grey_1)};
-    margin-bottom: 33px;
-    justify-content: center;
-    align-items: center;
-    padding: 16px;
+  width: 311px;
+  height: 175px;
+  border-radius: 12px;
+  background: ${({ isDark }) => (isDark ? colors.grey_8 : colors.grey_1)};
+  margin-bottom: 33px;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
 `
 
 const RecText1 = styled.Text`
-    color: ${({ isDark }) => (isDark ? colors.grey_3 : colors.grey_7)};
-    font-size: 13px;
-    font-family: Pretendard-Regular;
-    line-height: 19.5px;
-    width: 188px;
+  color: ${({ isDark }) => (isDark ? colors.grey_3 : colors.grey_7)};
+  font-size: 13px;
+  font-family: Pretendard-Regular;
+  line-height: 19.5px;
+  width: 188px;
 `
 
 const RecText2 = styled.Text`
-    color: ${({ isDark }) => (isDark ? colors.grey_3 : colors.grey_7)};
-    font-size: 13px;
-    font-family: Pretendard-Regular;
-    line-height: 19.5px;
-    width: 55px;
+  color: ${({ isDark }) => (isDark ? colors.grey_3 : colors.grey_7)};
+  font-size: 13px;
+  font-family: Pretendard-Regular;
+  line-height: 19.5px;
+  width: 55px;
 `
 
 const RecText3 = styled.Text`
-    color: ${({ isDark }) => (isDark ? colors.grey_3 : colors.grey_7)};
-    font-size: 13px;
-    font-family: Pretendard-Regular;
-    line-height: 19.5px;
-    width: 36px;
+  color: ${({ isDark }) => (isDark ? colors.grey_3 : colors.grey_7)};
+  font-size: 13px;
+  font-family: Pretendard-Regular;
+  line-height: 19.5px;
+  width: 36px;
 `
 
 const ExerciseButtonText = styled.Text`
-    color: ${({ isDark }) => (isDark ? colors.black : colors.white)};
-    text-align: center;
-    font-size: 24px;
-    font-family: Pretendard-SemiBold;
-    line-height: 33.6px;
+  color: ${({ isDark }) => (isDark ? colors.black : colors.white)};
+  text-align: center;
+  font-size: 24px;
+  font-family: Pretendard-SemiBold;
+  line-height: 33.6px;
 `
 
 const ExerciseExplainText = styled.Text`
-    padding: 8px;
-    color: ${colors.l_main};
-    text-align: center;
-    font-size: 13px;
-    font-family: Pretendard-Regular;
-    line-height: 19.5px;
-    margin-bottom: 41px;
+  padding: 8px;
+  color: ${colors.l_main};
+  text-align: center;
+  font-size: 13px;
+  font-family: Pretendard-Regular;
+  line-height: 19.5px;
+  margin-bottom: 41px;
 `
 
 const Container2 = styled.View`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
-    padding: 0px 23.5px;
-    background: ${({ isDark }) => (isDark ? colors.grey_8 : colors.grey_1)};
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 23.5px;
+  background: ${({ isDark }) => (isDark ? colors.grey_8 : colors.grey_1)};
 `
 
 const ExerciseCircle = styled.View`
-    width: 307px;
-    height: 307px;
-    border-radius: 291px;
-    background: ${({ isDark }) => (isDark ? colors.black : colors.white)};
-    margin-bottom: 24px;
-    justify-content: center;
-    align-items: center;
+  width: 307px;
+  height: 307px;
+  border-radius: 291px;
+  background: ${({ isDark }) => (isDark ? colors.black : colors.white)};
+  margin-bottom: 24px;
+  justify-content: center;
+  align-items: center;
 `
 
 const BackIcon = styled.TouchableOpacity`
-    position: absolute;
-    left: 24px;
-    top: 16px;
+  position: absolute;
+  left: 24px;
+  top: 16px;
 `
 
 export default function StartExercise({ navigation }) {
-    const isFocused = useIsFocused()
-    const [isTabVisible, setIsTabVisible] = useRecoilState(TabBarAtom)
-    const isDark = useRecoilValue(IsDarkAtom)
+  const isFocused = useIsFocused()
+  const [isTabVisible, setIsTabVisible] = useRecoilState(TabBarAtom)
+  const isDark = useRecoilValue(IsDarkAtom)
 
-    const Week = new Array('sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat')
+  const Week = new Array('sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat')
 
-    const now = new Date()
-    let day = Week[now.getDay()]
+  const now = new Date()
+  let day = Week[now.getDay()]
 
-    const goToExerciseCourse = () =>
-        navigation.navigate('ExerciseCourse', {
-            dataList: dataList,
-            listIndex: 0,
-            routineIdx: routineIdx,
-            totalTime: 0,
-        })
+  const goToExerciseCourse = () =>
+    navigation.navigate('ExerciseCourse', {
+      dataList: dataList,
+      listIndex: 0,
+      routineIdx: routineIdx,
+      totalTime: 0,
+    })
 
-    const [dataList, setDataList] = useState([])
-    const [circleList, setCircleList] = useState([])
+  const [dataList, setDataList] = useState([])
+  const [circleList, setCircleList] = useState([])
 
-    const getExerciseData = async (day) => {
-        try {
-            let url = 'https://gpthealth.shop/'
-            let detailAPI = `/app/process/`
+  const getExerciseData = async (day) => {
+    try {
+      let url = 'https://gpthealth.shop/'
+      let detailAPI = `/app/process/`
 
-            const response = await axios.get(url + detailAPI, {
-                params: {
-                    dayOfWeek: day,
-                },
-            })
-            const result = response.data
-            console.log(result)
-            return result
-        } catch (error) {
-            console.error('Error', error)
-        }
+      const response = await axios.get(url + detailAPI, {
+        params: {
+          dayOfWeek: day,
+        },
+      })
+      const result = response.data
+      console.log(result)
+      return result
+    } catch (error) {
+      console.error('Error', error)
     }
+  }
 
-    const [isLoading, setIsLoading] = useState(true)
-    const [isNoRoutine, setIsNoRoutine] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [isNoRoutine, setIsNoRoutine] = useState(false)
 
-    const getRoutineData = async () => {
-        try {
-            let url = 'https://gpthealth.shop/'
-            let detailAPI = `/app/routine/calendar`
+  const getRoutineData = async () => {
+    try {
+      let url = 'https://gpthealth.shop/'
+      let detailAPI = `/app/routine/calendar`
 
-            const response = await axios.get(url + detailAPI)
-            const result = response.data
-            return result
-        } catch (error) {
-            console.error('Error', error)
-        }
+      const response = await axios.get(url + detailAPI)
+      const result = response.data
+      return result
+    } catch (error) {
+      console.error('Error', error)
     }
+  }
 
-    //fri,mon,sat,sun,thu,tue,wed
-    let day2 = (now.getDay() + 6) % 7
+  //fri,mon,sat,sun,thu,tue,wed
+  let day2 = (now.getDay() + 6) % 7
 
-    useEffect(() => {
-        setIsLoading(true)
+  useEffect(() => {
+    setIsLoading(true)
 
-        async function fetchData() {
-            const routineData = await getRoutineData()
+    async function fetchData() {
+      const routineData = await getRoutineData()
 
-            if (routineData.isSuccess === false) {
-                navigation.dispatch(StackActions.replace('RegisterRoutine'))
-                return
-            } else {
-                const dayRoutineArr = processDayData(routineData.result)
-                const dayRoutineIdx = dayRoutineArr[day2].routineId
-                console.log(dayRoutineIdx)
-                if (dayRoutineIdx === 0) {
-                    setIsNoRoutine(true)
-                    setIsLoading(false)
-                    return
-                } else {
-                    setIsNoRoutine(false)
-                    isFocused && setIsTabVisible(false)
-                    getExerciseData(day).then((response) => {
-                        setDataList(response.result.routineDetails)
-                        setCircleList(response.result)
-                    })
-                }
-            }
-            setIsLoading(false)
+      if (routineData.isSuccess === false) {
+        navigation.dispatch(StackActions.replace('RegisterRoutine'))
+        return
+      } else {
+        const dayRoutineArr = processDayData(routineData.result)
+        const dayRoutineIdx = dayRoutineArr[day2].routineId
+        console.log(dayRoutineIdx)
+        if (dayRoutineIdx === 0) {
+          setIsNoRoutine(true)
+          setIsLoading(false)
+          return
+        } else {
+          setIsNoRoutine(false)
+          isFocused && setIsTabVisible(false)
+          getExerciseData(day).then((response) => {
+            setDataList(response.result.routineDetails)
+            setCircleList(response.result)
+          })
         }
-        fetchData()
-    }, [isFocused, navigation, setIsTabVisible])
-
-    const routineIdx = circleList?.routineIdx
-
-    const exerciseList = dataList.map((result) => (
-        <RecTextLine key={result.exerciseInfo.healthCategoryIdx}>
-            <RecText1 isDark={isDark}>{result.exerciseInfo.exerciseName}</RecText1>
-            <RecText2 isDark={isDark} />
-            <RecText3 isDark={isDark}>{result.totalSets}세트</RecText3>
-        </RecTextLine>
-    ))
-
-    if (isLoading) {
-        return <Loading />
-    } else if (isNoRoutine) {
-        return <NoRoutine />
-    } else {
-        return (
-            <SafeAreaView
-                style={{
-                    flex: 1,
-                    backgroundColor: isDark ? colors.d_background : colors.white,
-                }}
-            >
-                <Container isDark={isDark}>
-                    <BackIcon onPress={() => navigation.goBack()}>
-                        <Left width={24} height={24} color={isDark ? colors.white : colors.black} />
-                    </BackIcon>
-                    <ExerciseText isDark={isDark}>운동을 시작해 볼까요?</ExerciseText>
-                    <ExerciseExplainText isDark={isDark}> </ExerciseExplainText>
-
-                    <CirclesLine>
-                        <ProgressCircle
-                            num={Math.ceil(circleList?.totalTime / 60)}
-                            unit="분"
-                            title="예상 소요시간"
-                            bubbleOn={false}
-                            isDark={isDark}
-                        />
-                        <ProgressCircle num="30" unit="초" title="세트간 휴식" bubbleOn={false} isDark={isDark} />
-
-                        <GrayCircle
-                            num={circleList?.totalCalories}
-                            unit="kcal"
-                            title="소모 칼로리"
-                            bubbleOn={false}
-                            isDark={isDark}
-                        />
-                    </CirclesLine>
-
-                    <ExerciseRec isDark={isDark}>
-                        <ScrollView>{exerciseList}</ScrollView>
-                    </ExerciseRec>
-
-                    <ExerciseButton isDark={isDark} onPress={goToExerciseCourse}>
-                        <ExerciseButtonText isDark={isDark}>시작</ExerciseButtonText>
-                    </ExerciseButton>
-                </Container>
-            </SafeAreaView>
-        )
+      }
+      setIsLoading(false)
     }
+    fetchData()
+  }, [isFocused, navigation, setIsTabVisible])
+
+  const routineIdx = circleList?.routineIdx
+
+  const exerciseList = dataList.map((result) => (
+    <RecTextLine key={result.exerciseInfo.healthCategoryIdx}>
+      <RecText1 isDark={isDark}>{result.exerciseInfo.exerciseName}</RecText1>
+      <RecText2 isDark={isDark} />
+      <RecText3 isDark={isDark}>{result.totalSets}세트</RecText3>
+    </RecTextLine>
+  ))
+
+  if (isLoading) {
+    return <Loading />
+  } else if (isNoRoutine) {
+    return <NoRoutine />
+  } else {
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: isDark ? colors.d_background : colors.white,
+        }}
+      >
+        <Container isDark={isDark}>
+          <BackIcon onPress={() => navigation.goBack()}>
+            <Left width={24} height={24} color={isDark ? colors.white : colors.black} />
+          </BackIcon>
+          <ExerciseText isDark={isDark}>운동을 시작해 볼까요?</ExerciseText>
+          <ExerciseExplainText isDark={isDark}> </ExerciseExplainText>
+
+          <CirclesLine>
+            <ProgressCircle
+              num={Math.ceil(circleList?.totalTime / 60)}
+              unit="분"
+              title="예상 소요시간"
+              bubbleOn={false}
+              isDark={isDark}
+            />
+            <ProgressCircle num="30" unit="초" title="세트간 휴식" bubbleOn={false} isDark={isDark} />
+
+            <GrayCircle
+              num={circleList?.totalCalories}
+              unit="kcal"
+              title="소모 칼로리"
+              bubbleOn={false}
+              isDark={isDark}
+            />
+          </CirclesLine>
+
+          <ExerciseRec isDark={isDark}>
+            <ScrollView>{exerciseList}</ScrollView>
+          </ExerciseRec>
+
+          <ExerciseButton isDark={isDark} onPress={goToExerciseCourse}>
+            <ExerciseButtonText isDark={isDark}>시작</ExerciseButtonText>
+          </ExerciseButton>
+        </Container>
+      </SafeAreaView>
+    )
+  }
 }
