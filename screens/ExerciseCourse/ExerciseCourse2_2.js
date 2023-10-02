079d9124a975dfa8fce57084278dd1e6dcfd7e58
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2023-10-03 02:49:04 +0900

    Update ExerciseCourse2_2.js

--- screens/ExerciseCourse/ExerciseCourse2_2.js
+++ screens/ExerciseCourse/ExerciseCourse2_2.js
@@ -130,7 +130,7 @@ export default function ExerciseCourse_2_2({
           }
         />
         <AdBox>
-          <AdView type="video" media={true} />
+          <AdView isSetGap={true} type="video" media={true} />
         </AdBox>
         {/* <ExerciseButton //운동 시작 버튼
           text="바로 시작하기"
